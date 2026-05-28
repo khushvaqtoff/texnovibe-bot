38054b853aaf6824ed60248156507572e98618be
khushvaqtoff <khushvaqtovo8@gmail.com>
2026-05-28 11:53:12 +0500

    query_handler syntax xato tuzatildi

--- handlers/query_handler.py
+++ handlers/query_handler.py
@@ -29,10 +29,9 @@ async def start_search(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return await search_query(update, context)
 
     await update.message.reply_text(
-        "🔍 *Qidirish*
+        "🔍 Qidirish
 
-"
-        "Ism yoki telefon raqamini yozing:",
+Ism yoki telefon raqamini yozing:",
         parse_mode="Markdown"
     )
     return SEARCH_QUERY
