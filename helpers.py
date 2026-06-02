46d39597824d530eabee37ba94eabe3fd9fd1acc
khushvaqtoff <khushvaqtovo8@gmail.com>
2026-06-02 20:04:53 +0500

    Final cleanup: Remove circular import and fix indent

--- helpers.py
+++ helpers.py
@@ -1,10 +1,3 @@
-from helpers import normalize_phone
-from sheets.google_sheets import add_sale, check_duplicate
-
-def handle_sale_process(user_data):
-    raw_phone = user_data.get('phone')
-    clean_phone = normalize_phone(raw_phone)
-    
-    # Endi baza bilan ishlash
-    if not check_duplicate(clean_phone):
-        add_sale([user_data['name'], clean_phone, user_data['job']])
+# helpers.py fayli faqat mana shunday bo'lsin:
+def normalize_phone(phone: str) -> str:
+    return str(phone).replace("+", "").replace(" ", "").replace("-", "").strip()
